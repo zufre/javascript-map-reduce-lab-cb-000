5b60e3dbce12a46b1090eb54e61013c6c9f182ee
Cedric David <cedric.david@btopenworld.com>
2018-10-05 15:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9005,6 +9005,6 @@ var issuesWithUpdatedApiUrl = issues.map((issue) => {
   return Object.assign({}, issue, {url: newUrl});
 });
 
-var 
+var
 commentCountAcrossIssues = issues.map( (issue) => issue.comments_count
   .reduce( (commentTot, count) => commentTot + count, 0);
